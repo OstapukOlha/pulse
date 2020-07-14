bc1d851951dbb0b8ea2f4f6555adc050d6f4fcef
Olya <ostapukolia10@gmail.com>
2020-07-14 12:41:34 +0300

    added all image

--- src/js/script.js
+++ src/js/script.js
@@ -89,4 +89,21 @@ $(document).ready(function(){
         });
         return false;
     });
+
+    // smooth scroll
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1100) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+    
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+    new WOW().init();
 });
